90ca73646612f6efe0b95c89245fcf99b2ee7a45
Hamid Ali <hammali99@gmail.com>
2022-07-19 01:27:52 +0500

    Resolved conflicts and issues

--- my.js
+++ my.js
@@ -1,14 +1,15 @@
 const hamburger = document.querySelector("#beautiful");
 const navMenu = document.querySelector("#hidden");
-
+const logo = document.querySelector(".wel");
 
 beautiful.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-
+    wel.classList.toggle("active");
 })
 
-document.querySelectorAll(".pb, .ab, .cb").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
