268666f08ad958838a289f0bb0d598b72550afd4
mj-e-boc <92397843+mj-e-boc@users.noreply.github.com>
2022-07-20 13:31:55 +0200

    add index.js for popup

--- my.js
+++ my.js
@@ -10,7 +10,7 @@ const projectHead = document.querySelectorAll(".modal-head")
 beautiful.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    
+
 })
 menu.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -21,7 +21,40 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
     nextHamburger.classList.remove("active");
-    
+
 }))
 
 
+openModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+overlay.addEventListener("click", () => {
+    const modals = document.querySelectorAll(".modal.active")
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = button.closest(".modal");
+        closeModal(modal);
+    })
+})
+
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+}
