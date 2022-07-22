9c5e6192722a481ac26b1b31651ae5175c83f4f2
@M Jay <apple@Mj-uBangane.local>
2022-07-22 18:20:08 +0200

    add js files for reserved data

--- my.js
+++ my.js
@@ -218,3 +218,30 @@ form.addEventListener('click', (event) => {
     emailError.classList.remove('show-error');
   }, 2500);
 });
+
+// irrelevant infor below 
+
+const form = document.getElementById('subit');
+const email = document.getElementsById('email');
+const emailError = document.getElementById('emailError');
+
+form.addEventListener('click', (event) => {
+  const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  const str = email.value;
+  if (str === str.toLowerCase()) {
+    if (regex.test(str)) {
+      emailError.classList.remove('show-error');
+    } else {
+      event.preventDefault();
+      emailError.classList.add('show-error');
+      emailError.textContent = 'You entered an invalid email';
+    }
+  } else {
+    event.preventDefault();
+    emailError.classList.add('show-error');
+    emailError.innerHTML = 'Email must be typed in lowercase';
+  }
+  setTimeout(() => {
+    emailError.classList.remove('show-error');
+  }, 2500);
+});
