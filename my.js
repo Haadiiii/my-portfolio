adbc416cac46ca5e2d93a747467c594bafe13743
Hamid Ali <hammali99@gmail.com>
2022-09-30 22:30:57 +0500

    new feature

--- my.js
+++ my.js
@@ -1,231 +1,236 @@
-const hamburger = document.querySelector('#beautiful');
-const navMenu = document.querySelector('#hidden');
-const nextHamburger = document.querySelector('#menu');
+const hamburger = document.querySelector("#beautiful");
+const navMenu = document.querySelector("#hidden");
+const nextHamburger = document.querySelector("#menu");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-nextHamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+nextHamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-  nextHamburger.classList.remove('active');
-}));
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    nextHamburger.classList.remove("active");
+  })
+);
 
 const modalInformation = {
-  heading1: 'Multi Post Stories',
-  title2: 'Keeping track of hundreds  of components website',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-  description2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  imageSrc: './images/todolist.png',
+  heading1: "Multi Post Stories",
+  title2: "Keeping track of hundreds  of components website",
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+  description2:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  imageSrc: "./images/todolist.png",
   button1: 'See Live  <img src="./images/icon.png">',
   button2: 'See Source <img src="./images/vector.png">',
-  li: 'html',
-  li2: 'Bootstrap',
-  li3: 'Ruby on rails',
+  li: "html",
+  li2: "Bootstrap",
+  li3: "Ruby on rails",
 };
 
-const div1 = document.createElement('div');
-div1.className = 'modal';
+const div1 = document.createElement("div");
+div1.className = "modal";
 document.body.appendChild(div1);
 
-const div = document.createElement('div');
-div.className = 'modal-header';
+const div = document.createElement("div");
+div.className = "modal-header";
 div1.appendChild(div);
 
-const div2 = document.createElement('div');
-div2.className = 'title';
+const div2 = document.createElement("div");
+div2.className = "title";
 div.appendChild(div2);
 
-const h2 = document.createElement('h1');
-h2.className = 'modal-headm';
+const h2 = document.createElement("h1");
+h2.className = "modal-headm";
 h2.textContent = modalInformation.heading1;
 div2.appendChild(h2);
 
-const h20 = document.createElement('div');
-h20.className = 'modal-header22';
+const h20 = document.createElement("div");
+h20.className = "modal-header22";
 div2.appendChild(h20);
 
-const h22 = document.createElement('h1');
-h22.className = 'modal-headd';
+const h22 = document.createElement("h1");
+h22.className = "modal-headd";
 h22.textContent = modalInformation.title2;
 h20.appendChild(h22);
 
-const span = document.createElement('button');
-span.className = 'close-button';
-span.innerHTML = '&times;';
+const span = document.createElement("button");
+span.className = "close-button";
+span.innerHTML = "&times;";
 div.appendChild(span);
 
-const div3 = document.createElement('div');
-div3.className = 'modal-tags';
+const div3 = document.createElement("div");
+div3.className = "modal-tags";
 div1.appendChild(div3);
 
-const ul = document.createElement('ul');
-ul.className = 'modal-social';
+const ul = document.createElement("ul");
+ul.className = "modal-social";
 div3.appendChild(ul);
 
-const li = document.createElement('li');
-li.className = 'tags1';
+const li = document.createElement("li");
+li.className = "tags1";
 li.innerHTML = modalInformation.li;
 ul.appendChild(li);
 
-const li2 = document.createElement('li');
-li2.className = 'tags2';
+const li2 = document.createElement("li");
+li2.className = "tags2";
 li2.innerHTML = modalInformation.li2;
 ul.appendChild(li2);
 
-const li3 = document.createElement('li');
-li3.className = 'tags3';
+const li3 = document.createElement("li");
+li3.className = "tags3";
 li3.innerHTML = modalInformation.li3;
 ul.appendChild(li3);
 
-const div4 = document.createElement('div');
-div4.className = 'modal-pic';
+const div4 = document.createElement("div");
+div4.className = "modal-pic";
 div1.appendChild(div4);
 
-const div12 = document.createElement('div');
-div12.className = 'modal-body';
+const div12 = document.createElement("div");
+div12.className = "modal-body";
 div4.appendChild(div12);
 
-const p = document.createElement('p');
-p.className = 'modal-mobile';
+const p = document.createElement("p");
+p.className = "modal-mobile";
 p.textContent = modalInformation.description2;
 div12.appendChild(p);
 
-const p2 = document.createElement('p');
-p2.className = 'modal-desktop';
+const p2 = document.createElement("p");
+p2.className = "modal-desktop";
 p2.textContent = modalInformation.description;
 div12.appendChild(p2);
 
-const div13 = document.createElement('div');
-div13.className = 'modal-button';
+const div13 = document.createElement("div");
+div13.className = "modal-button";
 div1.appendChild(div13);
 
-const button = document.createElement('button');
-button.className = 'live-button';
+const button = document.createElement("button");
+button.className = "live-button";
 button.innerHTML = modalInformation.button1;
 div13.appendChild(button);
 
-const button2 = document.createElement('button');
-button2.className = 'source-button';
+const button2 = document.createElement("button");
+button2.className = "source-button";
 button2.innerHTML = modalInformation.button2;
 div13.appendChild(button2);
 
-const parentModal = document.querySelector('.modal');
-const openModalButtons = document.querySelector('.btn');
-const closeModalButtons = document.querySelector('#close-btn-new');
-const closeModalButton = document.querySelector('.close-button');
-const overlay = document.querySelector('#main');
-const openModalButtons2 = document.querySelector('.btn1');
-const openModalButtons3 = document.querySelector('.btn2');
-const openModalButtons4 = document.querySelector('.btn3');
-const openModalButtons5 = document.querySelector('.btn4');
-const openModalButtons6 = document.querySelector('.btn5');
-const openModalButtons7 = document.querySelector('.btn6');
-const sixPic = document.getElementById('sixpic')
+const parentModal = document.querySelector(".modal");
+const openModalButtons = document.querySelector(".btn");
+const closeModalButtons = document.querySelector("#close-btn-new");
+const closeModalButton = document.querySelector(".close-button");
+const overlay = document.querySelector("#main");
+const openModalButtons2 = document.querySelector(".btn1");
+const openModalButtons3 = document.querySelector(".btn2");
+const openModalButtons4 = document.querySelector(".btn3");
+const openModalButtons5 = document.querySelector(".btn4");
+const openModalButtons6 = document.querySelector(".btn5");
+const openModalButtons7 = document.querySelector(".btn6");
+const sixPic = document.getElementById("sixpic");
 
 function appear() {
-  sixPic.style.display = 'grid';
+  sixPic.style.display = "grid";
   // overlay.style.filter = 'blur(10px)';
+  console.log("ok");
 }
 
-openModalButtons.addEventListener('click', appear);
+openModalButtons.addEventListener("click", appear);
 
 function disappear() {
-  sixPic.style.display = 'none';
-  overlay.style.filter = 'blur(0px)';
+  sixPic.style.display = "none";
+  overlay.style.filter = "blur(0px)";
 }
 function disappear2() {
-  parentModal.style.display = 'none';
-  overlay.style.filter = 'blur(0px)';
+  parentModal.style.display = "none";
+  overlay.style.filter = "blur(0px)";
 }
 
-closeModalButtons.addEventListener('click', disappear);
-closeModalButton.addEventListener('click', disappear2);
+closeModalButtons.addEventListener("click", disappear);
+closeModalButton.addEventListener("click", disappear2);
 
 function dissappearParent(e) {
-  if (e.target.className === '.modal') {
-    parentModal.style.display = 'none';
-    overlay.style.filter = 'blur(0px)';
+  if (e.target.className === ".modal") {
+    parentModal.style.display = "none";
+    overlay.style.filter = "blur(0px)";
   }
 }
-parentModal.addEventListener('click', dissappearParent);
+parentModal.addEventListener("click", dissappearParent);
 
 function appearr() {
-  parentModal.style.display = 'block';
-  overlay.style.filter = 'blur(10px)';
+  parentModal.style.display = "block";
+  overlay.style.filter = "blur(10px)";
 }
 
 function appearrr() {
-  parentModal.style.display = 'block';
-  overlay.style.filter = 'blur(10px)';
+  parentModal.style.display = "block";
+  overlay.style.filter = "blur(10px)";
 }
 
 function appearrrr() {
-  parentModal.style.display = 'block';
-  overlay.style.filter = 'blur(10px)';
+  parentModal.style.display = "block";
+  overlay.style.filter = "blur(10px)";
 }
 
 function appearrrrr() {
-  parentModal.style.display = 'block';
-  overlay.style.filter = 'blur(10px)';
+  parentModal.style.display = "block";
+  overlay.style.filter = "blur(10px)";
 }
 
 function appearrrrrr() {
-  parentModal.style.display = 'block';
-  overlay.style.filter = 'blur(10px)';
+  parentModal.style.display = "block";
+  overlay.style.filter = "blur(10px)";
 }
 
 function appearrrrrrr() {
-  parentModal.style.display = 'block';
-  overlay.style.filter = 'blur(10px)';
+  parentModal.style.display = "block";
+  overlay.style.filter = "blur(10px)";
 }
 
-openModalButtons7.addEventListener('click', appearrrrrrr);
-openModalButtons5.addEventListener('click', appearrrrr);
-openModalButtons6.addEventListener('click', appearrrrrr);
-openModalButtons4.addEventListener('click', appearrrr);
-openModalButtons3.addEventListener('click', appearrr);
-openModalButtons2.addEventListener('click', appearr);
+openModalButtons7.addEventListener("click", appearrrrrrr);
+openModalButtons5.addEventListener("click", appearrrrr);
+openModalButtons6.addEventListener("click", appearrrrrr);
+openModalButtons4.addEventListener("click", appearrrr);
+openModalButtons3.addEventListener("click", appearrr);
+openModalButtons2.addEventListener("click", appearr);
 
 // Form javascript
 
-const form = document.getElementById('subit');
-const email = document.getElementById('email');
-const emailError = document.getElementById('emailError');
+const form = document.getElementById("subit");
+const email = document.getElementById("email");
+const emailError = document.getElementById("emailError");
 
-form.addEventListener('click', (event) => {
+form.addEventListener("click", (event) => {
   const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
   const str = email.value;
   if (str === str.toLowerCase()) {
     if (regex.test(str)) {
-      emailError.classList.remove('show-error');
+      emailError.classList.remove("show-error");
     } else {
       event.preventDefault();
-      emailError.classList.add('show-error');
-      emailError.textContent = 'You entered an invalid email';
+      emailError.classList.add("show-error");
+      emailError.textContent = "You entered an invalid email";
     }
   } else {
     event.preventDefault();
-    emailError.classList.add('show-error');
-    emailError.innerHTML = 'Email must be typed in lowercase';
+    emailError.classList.add("show-error");
+    emailError.innerHTML = "Email must be typed in lowercase";
   }
   setTimeout(() => {
-    emailError.classList.remove('show-error');
+    emailError.classList.remove("show-error");
   }, 2500);
 });
 
 // Store data in localStorage
 
-const fullName = document.getElementById('name');
-const message = document.getElementById('txt-area');
-const reset = document.getElementById('reset');
+const fullName = document.getElementById("name");
+const message = document.getElementById("txt-area");
+const reset = document.getElementById("reset");
 
 function changeHandler() {
   const field = {
@@ -233,18 +238,31 @@ function changeHandler() {
     email: email.value,
     message: message.value,
   };
-  localStorage.setItem('user', JSON.stringify(field));
+  localStorage.setItem("user", JSON.stringify(field));
 }
-const user = JSON.parse(localStorage.getItem('user'));
+const user = JSON.parse(localStorage.getItem("user"));
 email.value = user.email;
 fullName.value = user.name;
 message.value = user.message;
 
 function resetFrom() {
-  email.value = '';
-  fullName.value = '';
-  message.value = '';
-  localStorage.removeItem('user');
+  email.value = "";
+  fullName.value = "";
+  message.value = "";
+  localStorage.removeItem("user");
 }
-fullName.addEventListener('onchange', changeHandler);
-reset.addEventListener('click', resetFrom);
+fullName.addEventListener("onchange", changeHandler);
+reset.addEventListener("click", resetFrom);
+
+new Typed(".auto-change", {
+  strings: [
+    " Web Developer",
+    " MERN Stack Developer",
+    " Full Stack Developer",
+    " Front-end Developer",
+    " React Native Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
